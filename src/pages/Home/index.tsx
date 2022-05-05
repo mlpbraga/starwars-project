e6331ee4ef586e95facb6f84636573ac742acf8a
Luísa Braga <luisa.braga@meliuz.com.br>
2022-05-04 20:17:10 -0400

    feat: load info from api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Starships from '../../services/starships';
+
+interface SpaceshipData {
+  uid: string;
+  name: string;
+  stops: number;
+}
 
 const Home: React.FC = () => {
-  return <div>HELLLOOO</div>;
+  const [distance, setDistance] = useState('');
+  const [spaceships, setSpaceships] = useState<Array<any>>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    const loadInfo = async (): Promise<void> => {
+      try {
+        const response = await Starships.list(500);
+        setSpaceships(response.starships);
+      } catch (error) {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadInfo();
+  }, []);
+  return (
+    <div>
+      {hasError && <>Erro</>}
+      {isLoading && <>Loading</>}
+      <ul>
+        {!hasError &&
+          !isLoading &&
+          spaceships &&
+          spaceships.map(item => (
+            <li key={item.uid}>
+              {item.name}: {item.stops}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/services/starships.ts
@@ -0,0 +1,54 @@
+import _ from 'lodash';
+import api from './api';
+
+interface StarshipListData {
+  uid: string;
+  name: string;
+  url: string;
+}
+
+interface StarshipDetailsData {
+  message: string;
+  result: {
+    properties: {
+      name: string;
+      MGLT: string;
+    };
+  };
+}
+
+const Starships = {
+  list: async (distance: number) => {
+    const response = await api.get('/starships');
+
+    const nextPage = response.data.next;
+    const previousPage = response.data.previous;
+    const starshipsList = response.data.results;
+    const idsList = starshipsList.map((item: StarshipListData) => item.uid);
+    const starshipsData = await Promise.all(
+      idsList.map((id: number) => Starships.get(id)),
+    );
+    if (!_.isEmpty(starshipsData)) {
+      const formattedStarship = starshipsData.map((item: any) => {
+        return {
+          uid: item.result.properties.uid,
+          name: item.result.properties.name,
+          stops: Math.ceil(distance / Number(item.result.properties.MGLT)),
+        };
+      });
+      return {
+        next: nextPage,
+        previous: previousPage,
+        starships: formattedStarship,
+      };
+    }
+    return {};
+  },
+  get: async (id: number): Promise<StarshipDetailsData> => {
+    const response = await api.get(`/starships/${id}`);
+
+    return response.data;
+  },
+};
+
+export default Starships;
