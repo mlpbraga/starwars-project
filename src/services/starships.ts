38fb39ed66d096e148decd577d2e89501d4e1e70
Luísa Braga <luisa.braga@meliuz.com.br>
2022-05-05 21:22:23 -0400

    refactor: spaceship data list

--- src/services/starships.ts
+++ src/services/starships.ts
@@ -19,27 +19,37 @@ interface StarshipDetailsData {
 }
 
 const Starships = {
-  list: async (page = '1') => {
+  list: async (distance: number, page = '1') => {
     const response = await api.get(`/starships?page=${page}&limit=15`);
 
     const nextPage = response.data.next;
     const previousPage = response.data.previous;
     const starshipsList = response.data.results;
-    const idsList = starshipsList.map((item: StarshipListData) => ({
-      uid: item.uid,
+    const idsList = starshipsList.map((item: StarshipListData) => item.uid);
+    const starshipsData = await Promise.all(
+      idsList.map((id: string) => Starships.get(id)),
+    );
+    if (!_.isEmpty(starshipsData)) {
+      const formattedStarship = starshipsData.map((item: any) => {
+        return {
+          uid: item.id,
       name: item.name,
-    }));
+          stops: Math.ceil(distance / Number(item.MGLT)),
+        };
+      });
 
     return {
       next: nextPage,
       previous: previousPage,
-      starships: idsList,
+        starships: formattedStarship,
     };
+    }
+    return {};
   },
-  get: async (id: number): Promise<StarshipDetailsData> => {
+  get: async (id: string): Promise<StarshipDetailsData> => {
     const response = await api.get(`/starships/${id}`);
 
-    return response.data;
+    return { id, ...response.data.result.properties };
   },
 };
 
