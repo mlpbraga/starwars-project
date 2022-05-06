0a0b1a1a63ad0a6a3cf3105a28974d889a5f528e
Luísa Braga <luisa.braga@meliuz.com.br>
2022-05-06 16:38:03 -0400

    refactor: remove cache

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,7 @@
-import { setupCache } from 'axios-cache-adapter';
 import axios from 'axios';
-
-const cache = setupCache({
-  maxAge: 30 * 60 * 1000,
-  debug: true,
-  exclude: {
-    query: false,
-  },
-});
 
 const api = axios.create({
   baseURL: 'https://swapi.dev/api/',
-  adapter: cache.adapter,
 });
 
 export default api;
