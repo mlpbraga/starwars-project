bcc068d25f92df18f9b6eb69ac36cf3c5167e8b2
Luísa Braga <luisa.braga@meliuz.com.br>
2022-05-05 21:19:12 -0400

    feat: adds cache to axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,17 @@
+import { setupCache } from 'axios-cache-adapter';
 import axios from 'axios';
 
+const cache = setupCache({
+  maxAge: 30 * 60 * 1000,
+  debug: true,
+  exclude: {
+    query: false,
+  },
+});
+
 const api = axios.create({
-  baseURL: 'https://www.swapi.tech/api/',
+  baseURL: 'https://www.swapi.dev/api/',
+  adapter: cache.adapter,
 });
 
 export default api;
